5027721cfdfff17f9b3de34188f47902c1c1a2ce
abdul-zignex <abdul@zignex.com>
2024-04-13 12:57:58 +0530

    Updated search to dropdowns for bus assignmnet

--- SchoolCab/app/src/main/java/com/example/schoolcab/AssignBusActivity.java
+++ SchoolCab/app/src/main/java/com/example/schoolcab/AssignBusActivity.java
@@ -7,43 +7,30 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.SetOptions;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class AssignBusActivity extends AppCompatActivity {
 
     private FirebaseFirestore db;
+    private AutoCompleteTextView busNoAutoComplete;
+    private List<String> busNumbers;
 
     public static final String SHARED_PREFS = "shared_prefs";
-
-    // key for schoolId
     public static final String sId = "sId";
-    // variable for shared preferences.
     SharedPreferences sharedpreferences;
 
     @Override
@@ -51,61 +38,80 @@ public class AssignBusActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assign_bus);
 
-        //        Getting the school id saved in local preferences
+        db = FirebaseFirestore.getInstance();
+        busNoAutoComplete = findViewById(R.id.autoCompleteBusNo);
+
         sharedpreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
         String schoolID = sharedpreferences.getString("sId", null);
 
+        populateBusNumbers(schoolID);
 
-        db = FirebaseFirestore.getInstance();
+        findViewById(R.id.search_button).setOnClickListener(v -> {
+            String selectedBusNo = busNoAutoComplete.getText().toString().trim();
+            if (!selectedBusNo.isEmpty()) {
+                searchBus(selectedBusNo, schoolID);
+            } else {
+                Toast.makeText(this, "Please select a bus number", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
-        Button searchButton = findViewById(R.id.search_button);
-        searchButton.setOnClickListener(v -> {
-            EditText editBusNo = findViewById(R.id.busNo);
-
-            int busNo = Integer.parseInt(editBusNo.getText().toString());
-
-            Log.d("Bus", "Name: " + busNo + schoolID);
-
-
-            db.collection("bus")
-                    .whereEqualTo("busNo", busNo)
-                    .whereEqualTo("schoolId",schoolID)
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if (task.isSuccessful()) {
-                                if (task.getResult().isEmpty()) {
-                                    Log.d(TAG, "Bus Not Found with Given Bus No. ");
-
-                                    Toast.makeText(AssignBusActivity.this, "BUS NOT FOUND WITH GIVEN BUS NO.", Toast.LENGTH_LONG).show();
-//                                    TextView textMessage = findViewById(R.id.textMessage);
-//                                    textMessage.setText("No Student Found with Given Enrollment No. Please Enter Again");
-
-
-                                } else {
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
-
-                                        Log.d(TAG, document.getId() + " => " + document.getData());
-
-                                        String jsonString = new Gson().toJson(document.getData());
-//                                        Toast.makeText(AssignBusActivity.this, "BUS FOUND", Toast.LENGTH_LONG).show();
-
-                                        Intent intent = new Intent(AssignBusActivity.this, UpdateRouteActivity.class);
-                                        intent.putExtra("data", jsonString);
-                                        intent.putExtra("id" ,document.getId());
-                                        startActivity(intent);
-
-                                    }
+    private void populateBusNumbers(String schoolID) {
+        db.collection("bus")
+                .whereEqualTo("schoolId", schoolID)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        busNumbers = new ArrayList<>();
+                        for (DocumentSnapshot document : task.getResult()) {
+                            Long busNo = document.getLong("busNo");
+                            if (busNo != null) {
+                                String busNoString = String.valueOf(busNo);
+                                if (!busNumbers.contains(busNoString)) {
+                                    busNumbers.add(busNoString);
                                 }
-                            } else {
-                                Log.d(TAG, "Error getting documents: ", task.getException());
                             }
                         }
-                    });
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, busNumbers);
+                        busNoAutoComplete.setAdapter(adapter);
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                    }
+                });
+    }
 
+    private void searchBus(String selectedBusNo, String schoolID) {
+        int busNoInt;
+        try {
+            busNoInt = Integer.parseInt(selectedBusNo);
+        } catch (NumberFormatException e) {
+            // Handle invalid input gracefully
+            Toast.makeText(this, "Invalid bus number", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        });
+        db.collection("bus")
+                .whereEqualTo("busNo", busNoInt)
+                .whereEqualTo("schoolId", schoolID)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        if (task.getResult().isEmpty()) {
+                            Toast.makeText(AssignBusActivity.this, "BUS NOT FOUND WITH GIVEN BUS NO.", Toast.LENGTH_LONG).show();
+                        } else {
+                            for (DocumentSnapshot document : task.getResult()) {
+//                                String jsonString = document.getData().toString();
+                                String jsonString = new Gson().toJson(document.getData());
+                                Intent intent = new Intent(AssignBusActivity.this, UpdateRouteActivity.class);
+                                intent.putExtra("data", jsonString);
+                                intent.putExtra("id", document.getId());
+                                startActivity(intent);
+                            }
+                        }
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                    }
+                });
+    }
 
-}
 }
--- SchoolCab/app/src/main/java/com/example/schoolcab/UpdateRouteActivity.java
+++ SchoolCab/app/src/main/java/com/example/schoolcab/UpdateRouteActivity.java
@@ -3,27 +3,22 @@ package com.example.schoolcab;
 import static android.content.ContentValues.TAG;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.helper.widget.MotionEffect;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.common.reflect.TypeToken;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
@@ -31,7 +26,6 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -42,117 +36,132 @@ import java.util.Map;
 public class UpdateRouteActivity extends AppCompatActivity {
 
     private FirebaseFirestore db;
+    private AutoCompleteTextView stopNameAutoComplete;
+    private List<String> stopNames;
 
     public static final String SHARED_PREFS = "shared_prefs";
-
-    // key for schoolId
     public static final String sId = "sId";
-    // variable for shared preferences.
+    Map<String, Object> jsonMap;
+    Map<String, Object> routeMap;
+    Map<String, Object> finalRouteMap;
     SharedPreferences sharedpreferences;
+    String id;
+    String jsonString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_route);
 
-        //        Getting the school id saved in local preferences
-        sharedpreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
-        String schoolID = sharedpreferences.getString("sId", null);
+        db = FirebaseFirestore.getInstance();
+        stopNameAutoComplete = findViewById(R.id.stopNameAutoComplete);
 
-        String jsonString = getIntent().getStringExtra("data");
-        String id = getIntent().getStringExtra("id");
+        jsonString = getIntent().getStringExtra("data");
+        id = getIntent().getStringExtra("id");
         // Log the JSON string
         Log.d(MotionEffect.TAG, "Received JSON data: " + jsonString);
 
         Gson gson = new Gson();
         Type type = new TypeToken<Map<String, Object>>() {}.getType();
-        Map<String, Object> jsonMap = gson.fromJson(jsonString, type);
-        Map<String, Object> routeMap = (Map<String, Object>) jsonMap.get("Route");
+        jsonMap = gson.fromJson(jsonString, type);
+        routeMap = (Map<String, Object>) jsonMap.get("Route");
         // Check if "Route" field is null
         if (routeMap == null) {
             // If "Route" field is null, create an empty Map for it
             routeMap = new HashMap<>();
         }
+        finalRouteMap = routeMap;
 
-
-        db = FirebaseFirestore.getInstance();
+        sharedpreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        String schoolID = sharedpreferences.getString("sId", null);
+        populateStopNames(schoolID);
 
         Button searchButton = findViewById(R.id.search_button);
-        Map<String, Object> finalRouteMap = routeMap;
         searchButton.setOnClickListener(v -> {
-            EditText editStopName = findViewById(R.id.stopName);
+            String selectedStopName = stopNameAutoComplete.getText().toString().trim();
+            if (!selectedStopName.isEmpty()) {
+                searchStop(selectedStopName, schoolID);
+            } else {
+                Toast.makeText(this, "Please select a stop name", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
-            String stopName = editStopName.getText().toString();
-
-            Log.d("Stop", "Name: " + stopName + schoolID);
-
-
-            db.collection("stops")
-                    .whereEqualTo("stopName", stopName)
-                    .whereEqualTo("schoolID",schoolID)
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if (task.isSuccessful()) {
-                                if (task.getResult().isEmpty()) {
-                                    Log.d(TAG, "Stop Not Found ");
-
-                                    Toast.makeText(UpdateRouteActivity.this, "BUS STOP NOT FOUND", Toast.LENGTH_LONG).show();
-//                                    TextView textMessage = findViewById(R.id.textMessage);
-//                                    textMessage.setText("No Student Found with Given Enrollment No. Please Enter Again");
-
-
-                                } else {
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
-
-                                        Log.d(TAG, document.getId() + " => " + document.getData());
-                                        GeoPoint geoPoint = document.getGeoPoint("location");
-                                        Double latitude = geoPoint.getLatitude();
-                                        Double longitude = geoPoint.getLongitude();
-
-                                        if (latitude != null && longitude != null) {
-                                            // Create a Map to represent location
-                                            Map<String, Object> locationMap = new HashMap<>();
-                                            locationMap.put("latitude", latitude);
-                                            locationMap.put("longitude", longitude);
-
-                                            // Add the stop to the "Route" map
-                                            finalRouteMap.put(stopName, locationMap);
-
-                                            // Update the "Route" field in the jsonMap
-                                            jsonMap.put("Route", finalRouteMap);
-
-                                            // Update the Bus document in Firestore
-                                            db.collection("bus").document(id)
-                                                    .set(jsonMap, SetOptions.merge())
-                                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                        @Override
-                                                        public void onSuccess(Void aVoid) {
-                                                            Log.d(TAG, "Route updated successfully");
-                                                            Toast.makeText(UpdateRouteActivity.this, "Route updated successfully", Toast.LENGTH_SHORT).show();
-                                                        }
-                                                    })
-                                                    .addOnFailureListener(new OnFailureListener() {
-                                                        @Override
-                                                        public void onFailure(@NonNull Exception e) {
-                                                            Log.w(TAG, "Error updating route", e);
-                                                            Toast.makeText(UpdateRouteActivity.this, "Error updating route", Toast.LENGTH_SHORT).show();
-                                                        }
-                                                    });
-                                        } else {
-                                            Log.e(TAG, "Latitude or longitude is null");
-                                        }
-                                    }
-                                }
-                            } else {
-                                Log.d(TAG, "Error getting documents: ", task.getException());
+    private void populateStopNames(String schoolID) {
+        db.collection("stops")
+                .whereEqualTo("schoolID", schoolID)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        stopNames = new ArrayList<>();
+                        for (DocumentSnapshot document : task.getResult()) {
+                            String stopName = document.getString("stopName");
+                            if (stopName != null) {
+                                stopNames.add(stopName);
                             }
                         }
-                    });
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, stopNames);
+                        stopNameAutoComplete.setAdapter(adapter);
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                    }
+                });
+    }
 
+    private void searchStop(String selectedStopName, String schoolID) {
+        db.collection("stops")
+                .whereEqualTo("stopName", selectedStopName)
+                .whereEqualTo("schoolID", schoolID)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        if (task.getResult().isEmpty()) {
+                            Toast.makeText(UpdateRouteActivity.this, "BUS STOP NOT FOUND", Toast.LENGTH_LONG).show();
+                        } else {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
 
-        });
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                GeoPoint geoPoint = document.getGeoPoint("location");
+                                Double latitude = geoPoint.getLatitude();
+                                Double longitude = geoPoint.getLongitude();
 
+                                if (latitude != null && longitude != null) {
+                                    // Create a Map to represent location
+                                    Map<String, Object> locationMap = new HashMap<>();
+                                    locationMap.put("latitude", latitude);
+                                    locationMap.put("longitude", longitude);
+
+                                    // Add the stop to the "Route" map
+                                    finalRouteMap.put(selectedStopName, locationMap);
+
+                                    // Update the "Route" field in the jsonMap
+                                    jsonMap.put("Route", finalRouteMap);
+
+                                    // Update the Bus document in Firestore
+                                    db.collection("bus").document(id)
+                                            .set(jsonMap, SetOptions.merge())
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Log.d(TAG, "Route updated successfully");
+                                                    Toast.makeText(UpdateRouteActivity.this, "Route updated successfully", Toast.LENGTH_SHORT).show();
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Log.w(TAG, "Error updating route", e);
+                                                    Toast.makeText(UpdateRouteActivity.this, "Error updating route", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                } else {
+                                    Log.e(TAG, "Latitude or longitude is null");
+                                }
+                            }
+                        }
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                    }
+                });
     }
 }
